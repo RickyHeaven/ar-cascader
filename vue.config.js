07864be63c75f85645cd287d13b2c523ef83010a
ricky <zhangqingcq@foxmail.com>
2021-12-31 09:11:04 +0800

    输出文件支持GZ压缩，去除map文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const compressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
   devServer: {
     open: true
@@ -8,5 +10,51 @@ module.exports = {
       template: 'public/index.html',
       filename: 'index.html'
     }
-  }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        output: {
+          libraryExport: 'default'
+        },
+        externals: {
+          vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+          },
+          'view-design': {
+            root: 'ViewUI',
+            commonjs: 'view-design',
+            commonjs2: 'view-design',
+            amd: 'view-design'
+          }
+        },
+        plugins: [
+          new compressionPlugin({
+            test: /\.js$|\.html$|\.css/, //匹配文件名
+            threshold: 10240, //对超过10kb的数据进行压缩
+            deleteOriginalAssets: false //是否删除原文件
+          })
+        ]
+      }
+    }
+    else {
+      return {
+        output: {
+          libraryExport: 'default'
+        },
+        plugins: [
+          new compressionPlugin({
+            test: /\.js$|\.html$|\.css/, //匹配文件名
+            threshold: 10240, //对超过10kb的数据进行压缩
+            deleteOriginalAssets: false //是否删除原文件
+          })
+        ]
+      }
+    }
+  },
+  
+  productionSourceMap: false
 }
